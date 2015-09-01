4e8d60dc310351aabc8151ddef683a3ae3d0a44a
Phil Cooper-King <phil@cooperking.net>
2015-09-01 13:55:34 +0100

    Starting to add schedular so I can add some logic for callbacks etc.

---
+++ code/core/schedular/schedular.cpp
@@ -0,0 +1,28 @@
+#include <core/schedular/schedular.hpp>
+
+
+namespace Core {
+namespace Schedular {
+
+
+bool
+initialize()
+{
+  return false;
+}
+
+
+void
+think()
+{
+}
+
+
+void
+add_delta_time_think()
+{
+}
+
+
+} // ns
+} // ns
---
+++ code/core/schedular/schedular.hpp
@@ -0,0 +1,25 @@
+#ifndef SCHEDULAR_INCLUDED_EBB5C06B_871B_4965_9DC2_A23D6699C80A
+#define SCHEDULAR_INCLUDED_EBB5C06B_871B_4965_9DC2_A23D6699C80A
+
+
+namespace Core {
+
+
+class Schedule
+{
+}; // class
+
+namespace Schedular {
+
+bool        initialize();
+void        think();
+
+void        add_delta_time_think();
+
+
+} // ns
+
+} // ns
+
+
+#endif // inc guard
