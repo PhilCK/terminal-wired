4df05fd8d58a89ebbdd9fe2d75c82d60d9b1d22b
Phil Cooper-King <phil@cooperking.net>
2015-09-15 13:42:17 +0100

    Better however rotations seem to kill movement.

--- code/systems/actor/actor.hpp
+++ code/systems/actor/actor.hpp
@@ -3,6 +3,7 @@
 
 
 #include <core/entity/entity.hpp>
+#include <math/mat/mat4.hpp>
 
 
 namespace Actor {
@@ -11,6 +12,7 @@ namespace Actor {
 void        turn_right(const Core::Entity e, const float turn);
 void        move_forward(const Core::Entity e, const float fwd);
 void        jump(const Core::Entity e, const float jmp);
+bool        get_view_matrix(const Core::Entity e, math::mat4 &out_view_mat);
 
 
 } // ns
