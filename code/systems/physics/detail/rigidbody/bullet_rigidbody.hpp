cfd777c22041dd7ab4311ca6bdd82dab03e0caa8
Phil Cooper-King <phil@cooperking.net>
2015-09-18 18:53:45 +0100

    Started on physics refactor to bring in 'worlds'

---
+++ code/systems/physics/detail/rigidbody/bullet_rigidbody.hpp
@@ -0,0 +1,21 @@
+#ifndef BULLET_RIGIDBODY_INCLUDED_3E32FEF2_4044_4461_A7A9_516DB67955F3
+#define BULLET_RIGIDBODY_INCLUDED_3E32FEF2_4044_4461_A7A9_516DB67955F3
+
+
+namespace Detail {
+
+
+class Bullet_rigidbody
+{
+public:
+
+  explicit Bullet_rigidbody();
+  
+
+}; // class
+
+
+} // ns
+
+
+#endif // inc guard
